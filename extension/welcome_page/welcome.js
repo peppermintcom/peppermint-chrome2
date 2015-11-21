eb5926fa09e287b3c59cf4ddb764f1165a9ff0eb
Matt Durr <admin@onedurr.com>
2015-11-20 18:20:18 -0800

    Added check on welcome page for a blocked mike, with basic alert to user on how to fix

--- extension/welcome_page/welcome.js
+++ extension/welcome_page/welcome.js
@@ -15,7 +15,14 @@
           },
 	        function(e) {
 	            console.log(e);
-	            document.querySelector("#errInfo").style.display = "block";
+				
+				if(e && e.name === "PermissionDeniedError") {
+					alert("Hey there! It looks like the microphone is blocked. \n\nTo fix, you'll need to click the video icon at the top right of this page, then select the 'Always allow' option. \n\nYou should only have to do this once and you'll be all set to use Peppermint!");
+					// todo: make into a pretty help dialog or page with screenshot of how to re-enable
+				}
+				
+				if (document.querySelector("#errInfo"))
+					document.querySelector("#errInfo").style.display = "block";
 	        }
 	    );
 	}
