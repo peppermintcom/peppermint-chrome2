1caf283c806b9d8753d8cc6ee0e9377883413838
Vlas Bashynskyy <bash.vlas@gmail.com>
2015-12-30 13:26:07 +0200

    wrapped the sender data getter into a try-catch + getting sender email from a user profile

--- extension/js/inceptor.js
+++ extension/js/inceptor.js
@@ -69,35 +69,50 @@
 				[	'-button-inserter-',	'/templates/button-inserter.html'		]
 			])
 			.then( function () {
+				
+				chrome.identity.getProfileUserInfo( function ( info ) {
+				
+					add_elements();
 
-				add_elements();
+					try {
 
-				var sender_data = {
-					sender_name: $(".gb_ab.gb_ea .gb_jb").text() || $(".gb_b.gb_2a.gb_R")[0].title.match( /\:([\s\S]+?)\(/ )[ 1 ].trim(),
-					sender_email: $(".gb_ab.gb_ea .gb_kb").text() || $(".gb_b.gb_2a.gb_R")[0].title.match( /\((.+?)\)/ )[ 1 ]
-				};
+						var sender_data = {
+							sender_name: $(".gb_ab.gb_ea .gb_jb").text() || $(".gb_b.gb_2a.gb_R")[0].title.match( /\:([\s\S]+?)\(/ )[ 1 ].trim(),
+							sender_email: $(".gb_ab.gb_ea .gb_kb").text() || $(".gb_b.gb_2a.gb_R")[0].title.match( /\((.+?)\)/ )[ 1 ]
+						};
+						
+					} catch ( e ) {
 
-				var event_hub = new EventHub();
+						var sender_data = {
+							sender_name: "",
+							sender_email: info.email
+						};
+					
+					}
 
-				var tooltip = new Tooltip( jQuery, id_to_template( "-tooltip-" ), $( "#peppermint_tooltip" )[0], url("/img"), event_hub );
-				new Popup( jQuery, id_to_template( "-popup-" ), $( "#peppermint_popup" )[0], url("/img"), event_hub );
-				new Timer( jQuery, id_to_template( "-timer-" ), $( "#peppermint_timer" )[0], event_hub );
-				new Player( jQuery, id_to_template( "-player-" ), $( "#peppermint_popup_player" )[0], url("/img") );
-				new Player( jQuery, id_to_template( "-player-" ), $( "#peppermint_mini_popup_player" )[0], url("/img") );
-				new MiniPopup( jQuery, id_to_template( "-mini-popup-" ), $( "#peppermint_mini_popup" )[0], url("/img"), event_hub );
-				new ButtonInserter( jQuery, !items["options_data"]["disable_reply_button"], id_to_template( "-button-inserter-" ), $( "#peppermint_button_inserter" )[0], url("/img"), event_hub );
+					var event_hub = new EventHub();
 
-				new GmailController(
-					new ContentRecorder( chrome.runtime, event_hub ),
-					new Uploader( jQuery.ajax, sender_data ),
-					event_hub,
-					chrome,
-					new LetterManager( jQuery, document, chrome, sender_data ),
-					jQuery,
-					tooltip,
-					new TranscriptionManager( jQuery, items["options_data"]["transcription_language"] ),
-					items["options_data"]["enable_immediate_insert"]
-				);
+					var tooltip = new Tooltip( jQuery, id_to_template( "-tooltip-" ), $( "#peppermint_tooltip" )[0], url("/img"), event_hub );
+					new Popup( jQuery, id_to_template( "-popup-" ), $( "#peppermint_popup" )[0], url("/img"), event_hub );
+					new Timer( jQuery, id_to_template( "-timer-" ), $( "#peppermint_timer" )[0], event_hub );
+					new Player( jQuery, id_to_template( "-player-" ), $( "#peppermint_popup_player" )[0], url("/img") );
+					new Player( jQuery, id_to_template( "-player-" ), $( "#peppermint_mini_popup_player" )[0], url("/img") );
+					new MiniPopup( jQuery, id_to_template( "-mini-popup-" ), $( "#peppermint_mini_popup" )[0], url("/img"), event_hub );
+					new ButtonInserter( jQuery, !items["options_data"]["disable_reply_button"], id_to_template( "-button-inserter-" ), $( "#peppermint_button_inserter" )[0], url("/img"), event_hub );
+
+					new GmailController(
+						new ContentRecorder( chrome.runtime, event_hub ),
+						new Uploader( jQuery.ajax, sender_data ),
+						event_hub,
+						chrome,
+						new LetterManager( jQuery, document, chrome, sender_data ),
+						jQuery,
+						tooltip,
+						new TranscriptionManager( jQuery, items["options_data"]["transcription_language"] ),
+						items["options_data"]["enable_immediate_insert"]
+					);
+
+				});
 
 			});
 
