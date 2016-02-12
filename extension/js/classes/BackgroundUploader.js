be5e454cb3f1f274245f6d77fc38c04a86d5a21e
Matt Durr <matt@onedurr.com>
2016-02-12 11:10:59 -0500

    logging and basic check on local storage

--- extension/js/classes/BackgroundUploader.js
+++ extension/js/classes/BackgroundUploader.js
@@ -2,16 +2,43 @@
 	function BackgroundUploader ( $, chrome, uploader ) {
 
 		var private = {
+            
+            inprogress: false,
 
             run: function ( callback ){
-                console.log('run');
+                console.log('background upload check - init');
+                
+                if(!private.inprogress){
+                    private.inprogress = true;
+                    
+                    try {
+                        private.start( callback );    
+                    } catch (error) {
+                        console.log(error);                        
+                        private.inprogress = false;
+                    }
+                }
+            },
+            
+            start: function( callback ) {
+                chrome.storage.local.get("peppermint-upload-queue", function(data){
+                    if(data && data.recordings){
+                        console.log('data found');
+                        console.log(data);
+                    } else {
+                        console.log('no uploads in queue');
+                        private.inprogress = false;
+                    }
+                })
             }			
 
 		};
 
 		( function constructor () {
             
-            setInterval(private.run, 500);
+            setInterval(private.run(function(){
+                console.log('backgound upload check - complete');
+            }), 500);
 
 			chrome.runtime.onMessage.addListener( function ( message, sender, callback ) {
 
