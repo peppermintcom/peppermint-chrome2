2c1b24e9c2c8f4726cfaf6ff16a05d393396d14d
Matt Durr <matt@onedurr.com>
2016-03-11 11:11:18 -0700

    metrics for options page

--- extension/options/options.js
+++ extension/options/options.js
@@ -1,33 +1,45 @@
 
 	( function ( $, chrome, utilities ) {
 		
-        function add_metric ( metric, log_result ){
+        var private = {
             
-            if(!utilities)
-                utilities = new Utilities( chrome, $, 'options' );
+            add_metric: function ( metric, log_result ){
                 
-            utilities.add_metric( metric, function ( result ) {
                 if(log_result)
-                    console.log({ metric, result });
-            });
-        }
+                    console.log('adding metric ' + metric.name + ' for element ' + metric.val.element);
+                
+                if(!utilities)
+                    utilities = new Utilities( chrome, $, 'options' );
+                    
+                utilities.add_metric( metric, function ( result ) {
+                    if(log_result)
+                        console.log({ metric, result });
+                });
+            }
+            
+        };
         
 		chrome.storage.local.get( null, function ( items ) {
 			
-			var browser_language = window.navigator.language;
 			var extension_transcription_language = null;
 			
-			console.log( "extension transcription enable_immediate_insert: " + items["options_data"]["enable_immediate_insert"] );
-			console.log( "extension transcription disable_reply_button: " + items["options_data"]["disable_reply_button"] );
-			
-			console.log( "browser language: " + browser_language);
-			console.log( "extension transcription langauge: " + items["options_data"]["transcription_language"] );
-
-			$("#ennable_immediate_insert")[0].checked = items["options_data"]["enable_immediate_insert"];
-			$("#disable_reply_button")[0].checked = items["options_data"]["disable_reply_button"];
-			
-			extension_transcription_language = (("transcription_language" in items["options_data"]))? items["options_data"]["transcription_language"] : browser_language;
+            var options_data = {
+                browser_language: window.navigator.language,
+                enable_immediate_insert: items["options_data"]["enable_immediate_insert"],
+                disable_reply_button: items["options_data"]["disable_reply_button"],
+                transcription_langauge: items["options_data"]["transcription_language"]
+            };
+            
+            extension_transcription_language = $.inArray('transcription_language', items["options_data"])
+                ? options_data.transcription_langauge : options_data.browser_language;
+                
+			$("#ennable_immediate_insert")[0].checked = options_data.enable_immediate_insert;
+			$("#disable_reply_button")[0].checked = options_data.disable_reply_button;
 			$('#transcription_language').val(extension_transcription_language);
+            
+            console.log({ 'options_data': options_data });
+            
+            private.add_metric({name:'options-data', val: { type: 'load', data: options_data } });
 
 		});
 
@@ -36,7 +48,10 @@
 			chrome.storage.local.get( null, function ( items ) {
 
 				items.options_data.disable_reply_button = event.target.checked;
-				chrome.storage.local.set({ options_data: items.options_data });
+				
+                chrome.storage.local.set({ options_data: items.options_data });
+                
+                private.add_metric({name:'options-data', val: { type: 'change', element: '#disable_reply_button', enabled: event.target.checked } });
 			
 			});
 
@@ -47,7 +62,10 @@
 			chrome.storage.local.get( null, function ( items ) {
 				
 				items.options_data.enable_immediate_insert = event.target.checked;
+                
 				chrome.storage.local.set({ options_data: items.options_data });
+                
+                private.add_metric({name:'options-data', val: { type: 'change', element: '#ennable_immediate_insert', enabled: event.target.checked } });
 			
 			});
 
@@ -58,14 +76,17 @@
 			chrome.storage.local.get( null, function ( items ) {
 				
 				items.options_data.transcription_language = event.target.options[event.target.selectedIndex].value;
+                
 				chrome.storage.local.set({ options_data: items.options_data });
+                
+                private.add_metric({name:'options-data', val: { type: 'change', element: '#transcription_language', value: items.options_data.transcription_language } });
 			
 			});
 
 		});
         
         ( function constructor () {
-    
+            
             private.add_metric({ name: 'class-load', val: { class: 'options' } });
 
         } () );
