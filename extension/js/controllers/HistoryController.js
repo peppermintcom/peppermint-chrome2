818975ecbc0466f2d4f4c4998ef8656fbfcc1fad
Vlas Bashynskyy <bash.vlas@gmail.com>
2016-03-28 21:10:04 +0300

    history fix

--- extension/js/controllers/HistoryController.js
+++ extension/js/controllers/HistoryController.js
@@ -13,7 +13,7 @@
 
 			chrome.runtime.sendMessage( { receiver: "GlobalController", name: "get_recording_data_arr" }, function ( recording_data_arr ) {
 
-				for ( var i = 0; i < recording_data_arr.length; i++ ) {
+				for ( var i = recording_data_arr.length; i--; ) {
 
 					$( "#history_items_container" ).append( history_item_factory.make_item( recording_data_arr[ i ] ) );
 
